69c6a28afd204353f3ea83adfd7cc9d782b94c1f
chrissharkey1 <113540916+chrissharkey1@users.noreply.github.com>
2023-03-12 20:49:34 -0400

    Lab 5 Commit

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -3,8 +3,19 @@
   by adding `<script src="script.js">` just before your closing `</body>` tag
 */
 
+function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery)
+  })
+}
+
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  const filterButton = document.querySelector('.filter_button')
+  let currentList = [];
+
   form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
     submitEvent.preventDefault(); // This prevents your page from going to http://localhost:3000/api even if your form still has an action set on it
     console.log('form submission'); // this is substituting for a "breakpoint"
@@ -16,10 +27,6 @@ async function mainEvent() { // the async keyword means we can make API requests
 
         Let's get those form results before sending off our GET request using the Fetch API
     */
-
-    // this is the preferred way to handle form data in JS in 2022
-    const formData = new FormData(submitEvent.target); // get the data from the listener target
-    const formProps = Object.fromEntries(formData); // Turn it into an object
 
     // You can also access all forms in a document by using the document.forms collection
     // But this will retrieve ALL forms, not just the one that "heard" a submit event - less good
@@ -51,11 +58,21 @@ async function mainEvent() { // the async keyword means we can make API requests
     */
 
     // This changes the response from the GET into data we can use - an "object"
-    const arrayFromJson = await results.json();
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+    currentList = await results.json();
+    console.table(currentList); // this is called "dot notation"
+    // currentList.data - we're accessing a key called 'data' on the returned object
     // it initially contains all 1,000 records from your request
   });
+
+  filterButton.addEventListener('click', (event) => {
+    const formData = new FormData(form);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+
+    console.log(newList);
+  })
 }
 
 /*
