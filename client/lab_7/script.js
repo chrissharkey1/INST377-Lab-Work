d9bdf964801a055b2ba520c335d30ecf280c7506
chrissharkey1 <113540916+chrissharkey1@users.noreply.github.com>
2023-04-03 03:28:26 -0400

    Lab 7 Final Commit

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -33,13 +33,18 @@ function cutList(list) {
 
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-  const filterButton = document.querySelector('#filter_button')
-  const loadDataButton = document.querySelector('#data_load')
-  const generateListButton = document.querySelector('#generate')
-  let currentList = [];
+  const filterButton = document.querySelector('#filter_button');
+  const loadDataButton = document.querySelector('#data_load');
+  const generateListButton = document.querySelector('#generate');
+  const textField = document.querySelector('#resto');
 
   const loadAnimation = document.querySelector('#data_load_animation');
   loadAnimation.style.display = 'none';
+  generateListButton.classList.add('hidden');
+  filterButton.classList.add('hidden');
+
+  let storedList = [];
+  let currentList = [];
 
   loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
     console.log('loading data'); // this is substituting for a "breakpoint"
@@ -47,18 +52,30 @@ async function mainEvent() { // the async keyword means we can make API requests
 
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
 
-    currentList = await results.json();
+    storedList = await results.json();
+    if (storedList.length > 0) {
+      generateListButton.classList.remove('hidden');
+      filterButton.classList.remove('hidden');
+    }
+
     loadAnimation.style.display = 'none';
 
-    console.table(currentList); // this is called "dot notation"
+    console.table(storedList); // this is called "dot notation"
   });
 
   filterButton.addEventListener('click', (event) => {
+    console.log('clicked filter')
     const formData = new FormData(form);
     const formProps = Object.fromEntries(formData);
+    let tempList = [];
 
     console.log(formProps);
-    const newList = filterList(currentList, formProps.resto);
+    if(currentList.length > 0) {
+      tempList = currentList;
+    } else {
+      tempList = storedList;
+    }
+    const newList = filterList(tempList, formProps.resto);
 
     console.log(newList);
     injectHTML(newList);
@@ -66,8 +83,16 @@ async function mainEvent() { // the async keyword means we can make API requests
 
   generateListButton.addEventListener('click', (event) => {
     console.log('generated list');
-    const restoList = cutList(currentList);
-    injectHTML(restoList);
+    currentList = cutList(storedList);
+    console.log(currentList);
+    injectHTML(currentList);
+  });
+
+  textField.addEventListener('input', (event) => {
+    console.log('input', event.target.value);
+    const newList = filterList(currentList, event.target.value);
+    console.log(newList);
+    injectHTML(newList);
   });
 }
 
