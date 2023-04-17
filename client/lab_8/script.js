b836ec39ea43abb887fbf3bec359625e0100ad11
chrissharkey1 <113540916+chrissharkey1@users.noreply.github.com>
2023-04-16 21:52:13 -0400

    Lab 8 Final commit

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -31,21 +31,49 @@ function getRandomIntInclusive(min, max) {
     });
   }
   
+  function initMap() {
+    const carto = L.map('map').setView([38.98, -76.93], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(carto);
+    return carto;
+  }
+
+  function placeMarker(array, map) {
+    console.log('array for markers', array);
+
+    map.eachLayer((layer) => {
+        if (layer instanceof L.Marker) {
+          layer.remove();
+        }
+      });
+
+    array.forEach((item) => {
+        console.log('placeMarker', item);
+        const {coordinates} = item.geocoded_column_1;
+        L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    })
+  }
+
   async function mainEvent() { 
     const form = document.querySelector('.main_form'); 
 //    const filterButton = document.querySelector('#filter_button');
     const loadDataButton = document.querySelector('#data_load');
+    const clearDataButton = document.querySelector('#data_clear');
     const generateListButton = document.querySelector('#generate');
     const textField = document.querySelector('#resto');
   
     const loadAnimation = document.querySelector('#data_load_animation');
     loadAnimation.style.display = 'none';
     generateListButton.classList.add('hidden');
+
+    const carto = initMap();
   
     const storedData = localStorage.getItem('storedData');
-    const parsedData = JSON.parse(storedData);
+    let parsedData = JSON.parse(storedData);
 
-    if (parsedData.length > 0) {
+    if (parsedData?.length > 0) {
         generateListButton.classList.remove('hidden');
     }
 
@@ -59,7 +87,12 @@ function getRandomIntInclusive(min, max) {
   
       const storedList = await results.json();
       localStorage.setItem('storedData', JSON.stringify(storedList));
-  
+      parsedData = storedList;
+
+      if (parsedData?.length > 0) {
+        generateListButton.classList.remove('hidden');
+      }
+
       loadAnimation.style.display = 'none';
       console.table(storedList);
     });
@@ -69,6 +102,7 @@ function getRandomIntInclusive(min, max) {
       currentList = cutList(parsedData);
       console.log(currentList);
       injectHTML(currentList);
+      placeMarker(currentList, carto);
     });
   
     textField.addEventListener('input', (event) => {
@@ -76,6 +110,13 @@ function getRandomIntInclusive(min, max) {
       const newList = filterList(currentList, event.target.value);
       console.log(newList);
       injectHTML(newList);
+      placeMarker(newList, carto);
+    });
+
+    clearDataButton.addEventListener("click", (event) => {
+        console.log('clear browser data');
+        localStorage.clear();
+        console.log('localStorage clear', localStorage.getItem("storedData"));
     });
   }
   
